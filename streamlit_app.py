e0af986af0e13f934e96d0fad89b1fc86c6e1381
chrux3 <32092914+chrux3@users.noreply.github.com>
2021-12-30 18:28:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ def run_query(query):
     return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+#rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.distance_km}:")
+#for row in rows:
+#    st.write(f"{row.name} has a :{row.distance_km}:")
