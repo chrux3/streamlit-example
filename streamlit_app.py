14e6a25fc9ed4ec97165e4fdf665978f0172d0a4
chrux3 <32092914+chrux3@users.noreply.github.com>
2021-12-30 18:44:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,13 +15,21 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
+## @st.cache(ttl=600)
+## def run_query(query):
+##     rows = conn.execute(query, headers=1)
+##     return rows
+## 
+## sheet_url = st.secrets["private_gsheets_url"]
+## rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+st.title("Connect to Google Sheets")
+gsheet_url = st.secrets["private_gsheets_url"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
 
 # Print results.
 #for row in rows:
